b1679e0c2e97987b6e6fd5456efbe4eb4da8fd18
Abdalan Ismael <80339116+IsmaelBernardino@users.noreply.github.com>
2022-05-02 14:21:13 -0500

    delete

--- weekly_mission_4/fizzbuzz/lib/utils/Reader.js
+++
@@ -1,9 +0,0 @@
-class Reader{
-  static readJsonFile(path){
-    const fs = require("fs");
-    const rawdata = fs.readFileSync(path);
-    return JSON.parse(rawdata);
-  }
-}
-
-module.exports = Reader
--- weekly_mission_4/fizzbuzz/test/services/ExplorerService.test.js
+++
@@ -1,9 +0,0 @@
-const ExplorerService = require('../../lib/services/Explorerservice')
-
-describe('test explorer service', () => {
-  test('requeriendo calcular todos los explores', () => {
-    const explorers = [{mission: 'node'}]
-    const explorerInNode = ExplorerService.filterByMission(explorers, 'node')
-    expect(explorerInNode.length).toBe(1)
-  })
-})
